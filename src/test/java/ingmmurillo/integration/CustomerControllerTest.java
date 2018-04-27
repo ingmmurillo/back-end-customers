c70f12247b85e5e0de1e1e855803370701a65d43
Mauricio Murillo <mauricio2k4@gmail.com>
2018-04-27 18:18:24 -0500

    Fixing the build

--- src/test/java/ingmmurillo/integration/CustomerControllerTest.java
+++ src/test/java/ingmmurillo/integration/CustomerControllerTest.java
@@ -39,7 +39,7 @@ public class CustomerControllerTest {
         Customer customer = new Customer("Mauricio", "Murillo");
         given(customerRepository.findByLastName("Murillo")).willReturn(Optional.of(customer));
 
-        String customerFullName = subject.getCustomer("Mur");
+        String customerFullName = subject.getCustomer("Murillo");
 
         assertThat(customerFullName, is("Mauricio Murillo"));
     }
